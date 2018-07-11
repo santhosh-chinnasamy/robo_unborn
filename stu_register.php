b9a0303a0c68faa473b00a207410cf4f64417265
Santhosh <csesanthosh15@gmail.com>
2018-07-11 20:38:20 +0530

    db connection

--- stu_register.php
+++ stu_register.php
@@ -1,6 +1,19 @@
 <?php
-    mysql_connect('localhost','root','');
-    mysql_selectdb('ramanan');
+define('DB_NAME','ramanan');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+$link=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if(!$link)
+{
+die('Could not connect:'.mysqli_error());
+}
+$db_selected=mysqli_select_db(DB_NAME,$link);
+if(!$db_selected)
+{
+die('Can not use '.DB_NAME.':'.mysqli_error());
+}
+
   if(isset($_POST['submit']))
   {
       $STU_name = $_POST['stu_name'];
@@ -14,15 +27,15 @@
       $STU_inst = $_POST['stu_inst'];
       $STU_instname = $_POST['stu_instname'];
       $Mcheck = "SELECT * from robo_stu WHERE stu_email = ' $STU_email'";
-        $MResCheck = mysql_query($Mcheck);
-          if(mysql_num_rows($MResCheck) > 0)
+        $MResCheck = mysqli_query($link,$Mcheck);
+          if(mysqli_num_rows($MResCheck) > 0)
           {
             echo "Email Already Exits";
           }
           else {
               $insert = "INSERT INTO robo_stu(stu_name,stu_fname,stu_mname,stu_aadhar,stu_dob,stu_email,stu_mob,stu_gender,
               stu_inst,stu_instname) VALUES ('$STU_name',' $STU_fname ',' $STU_mname','$STU_aadhar','$STU_dob',' $STU_email',' $STU_mob',' $STU_gender','$STU_inst','  $STU_instname')";
-            $ResIns = mysql_query($insert); 
+            $ResIns = mysqli_query($link,$insert);
             echo "Inserted Successfully";
         }
   }
